aa5774f3f961372f6ad127b1e28c881284d76055
Christian <neocsr@gmail.com>
2016-08-18 08:14:16 -0700

    Add configuration support to user_key and api_version

--- app/services/better_doctor.rb
+++ app/services/better_doctor.rb
@@ -4,13 +4,14 @@ class BetterDoctor
 
   CONFIG = Rails.configuration.better_doctor
   include HTTParty
+  headers 'Accept' => 'application/json'
 
-  base_uri "#{CONFIG['host']}:#{CONFIG['port']}"
+  base_uri "#{CONFIG['host']}:#{CONFIG['port']}/#{CONFIG['api_version']}"
   open_timeout CONFIG['open_timeout']
   read_timeout CONFIG['read_timeout']
 
   def self.search(name)
-    resp = get("/search", {query: {name: name}})
+    resp = get("/doctors", {query: {name: name, user_key: CONFIG['user_key']}})
     resp.body
   rescue => e
     raise Error, "#{e.class}: #{e.message}"
--- test/services/better_doctor_test.rb
+++ test/services/better_doctor_test.rb
@@ -2,39 +2,43 @@ require 'test_helper'
 
 class BetterDoctorTest < ActiveSupport::TestCase
   test "search by name" do
-    stub_request(:get, "http://localhost:4000/search")
-      .with(query: {"name" => "John Doe"})
-      .to_return(body: '{"name": "John"}')
-
+    stub_request_with_name
     resp = BetterDoctor.search("John Doe")
 
-    assert_requested(:get, "http://localhost:4000/search?name=John%20Doe",
+    assert_requested(:get, "#{expected_base_url}?name=John%20Doe&user_key=demo",
       times: 1)
     assert_equal("{\"name\": \"John\"}", resp)
   end
 
   test "multiple cached searches by name make only one request" do
-    stub_request(:get, "http://localhost:4000/search")
-      .with(query: {"name" => "John Doe"})
-      .to_return(body: '{"name": "John"}')
-
+    stub_request_with_name
     BetterDoctor.cached_search("John Doe")
     BetterDoctor.cached_search("John Doe")
     resp = BetterDoctor.cached_search("John Doe")
 
-    assert_requested(:get, "http://localhost:4000/search?name=John%20Doe",
+    assert_requested(:get, "#{expected_base_url}?name=John%20Doe&user_key=demo",
       times: 1)
     assert_equal("{\"name\": \"John\"}", resp)
   end
 
   test "re-raise exceptions as BetterDoctor errors" do
-    stub_request(:get, "http://localhost:4000/search")
-      .with(query: {"name" => "John Doe"})
+    stub_request(:get, expected_base_url)
+      .with(query: {"name" => "John Doe", "user_key" => "demo"})
       .to_raise(Errno::ECONNREFUSED)
 
     assert_raises(BetterDoctor::Error) do
       BetterDoctor.search("John Doe")
     end
+  end
+
+  def stub_request_with_name
+    stub_request(:get, expected_base_url)
+      .with(query: {"name" => "John Doe", "user_key" => "demo"})
+      .to_return(body: '{"name": "John"}')
+  end
+
+  def expected_base_url
+    "http://localhost:4000/2016-03-01/doctors"
   end
 
 end
