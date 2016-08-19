31fef4cd113e6e8ef65b70a39a2effc06ad9964f
Christian <neocsr@gmail.com>
2016-08-18 23:16:10 -0700

    Add client error message on backend failure

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::DoctorsController < ApplicationController
+  rescue_from BetterDoctor::Error, with: :error_message
+
   def search
     render json: BetterDoctor.search(params[:name])
+  end
+
+  def error_message
+    render json: {message: "Backend error"}, status: :bad_gateway
   end
 end
--- test/controllers/api/v1/doctors_controller_test.rb
+++ test/controllers/api/v1/doctors_controller_test.rb
@@ -10,4 +10,13 @@ class Api::V1::DoctorsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "get search with error" do
+    BetterDoctor.expects(:search)
+      .raises(BetterDoctor::Error)
+    get :search, {name: "John Doe"}
+
+    assert_response :bad_gateway
+    assert_equal("{\"message\":\"Backend error\"}", @response.body)
+  end
+
 end
