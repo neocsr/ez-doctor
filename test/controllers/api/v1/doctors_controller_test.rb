88cc6506d4ecc9ac01fb5f29e0282e74e931f049
Christian <neocsr@gmail.com>
2016-08-18 22:55:43 -0700

    Update test with mocha

--- test/controllers/api/v1/doctors_controller_test.rb
+++ test/controllers/api/v1/doctors_controller_test.rb
@@ -3,13 +3,11 @@ require 'test_helper'
 class Api::V1::DoctorsControllerTest < ActionController::TestCase
 
   test "get search" do
-    stub_request(:get, "http://localhost:4000/2016-03-01/doctors?name=&user_key=demo")
-      .with(:headers => {'Accept'=>'application/json'})
-      .to_return(body: '{"name": "John"}')
-
-    get :search
+    BetterDoctor.expects(:search)
+      .with("John Doe")
+    get :search, {name: "John Doe"}
 
     assert_response :success
-    assert_equal("{\"name\": \"John\"}", @response.body)
   end
+
 end
